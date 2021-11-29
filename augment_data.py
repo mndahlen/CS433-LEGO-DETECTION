096ee6571efff6977ec4aba1135bb3d315a55771
Arvid Linder <arvli383@student.liu.se>
2021-11-29 22:26:32 +0100

    Added function for colour

--- augment_data.py
+++ augment_data.py
@@ -16,7 +16,6 @@ def flip_horizontal(image):
 def add_noise(image, mean, sigma):
     gauss = np.abs(np.random.normal(mean,sigma,img.shape))
     noisy = np.uint8(image + gauss)
-
     return noisy
 
 def gaussian_blur(image, kernel_size = (5,5), sigma=0):
@@ -28,6 +27,18 @@ def change_brigthness(img, factor):
     img[img > 255] = 255
     return np.uint8(img*factor)
 
+# Change colour of whole picture. USing HSV to only change the colour
+# Don't know how good it is, gets rather blurry around the edges
+# For some reason it can't change back??
+def change_colour(img, colour_add):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV_FULL)
+    img_hsv = img.copy()
+    for row in img_hsv:
+        for col in row:
+            if (col[0] != 0):
+                col[0] = (col[0] + colour_add) % 255
+    #img_hsv = cv2.cvtColor(img_hsv, cv2.COLOR_HSV2BGR_FULL)
+    return img_hsv
 
 IMGDIR = "data/kaggle/3003"
 img = cv2.imread(os.path.join(IMGDIR,"0001" + ".png"))
@@ -35,9 +46,10 @@ img_flipped = flip_horizontal(img)
 img_noise = add_noise(img, 0, 30)
 img_blur = gaussian_blur(img)
 img_brighter = change_brigthness(img, 1.2)
-
-img_combo = change_brigthness(gaussian_blur(add_noise(img, 0, 30)),0.8)
-cv2.imshow("test",img_noise)
+img_colour = change_colour(img, 150)
+img_combo = change_brigthness(add_noise(change_colour(img, 150), 0, 30),0.8)
+img_combo = cv2.undistort(img_combo, np.array([[2, 0, 0], [0, 1, 0], [3, 2, 1]]), None)
+cv2.imshow("test",img_combo)
 cv2.waitKey(0) 
 
 #closing all open windows 
