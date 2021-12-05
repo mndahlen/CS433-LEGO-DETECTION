160429653d0da907fdaf91736bb02d4a96c6c9be
martin dahl <marda545@student.liu.se>
2021-12-05 20:13:04 +0000

    Fixed weird looking names, removed some comments.

--- augment_data.py
+++ augment_data.py
@@ -19,16 +19,34 @@ def add_noise(image, mean, sigma):
     return noisy
 
 def gaussian_blur(image, kernel_size = (5,5), sigma=0):
-    blur = cv2.GaussianBlur(img,kernel_size,sigma)
+    blur = cv2.GaussianBlur(image,kernel_size,sigma)
     return blur
 
 def blur(image, kernel_size = (5,5)):
     return cv2.blur(image, kernel_size)
 
-def change_brigthness(img, factor):
-    img = img*factor
-    img[img > 255] = 255
-    return np.uint8(img*factor)
+def change_brigthness(image, factor):
+    image = image*factor
+    image[image > 255] = 255
+    return np.uint8(image*factor)
+
+def motion_blur(image, orientation, factor = 30):
+    kernel_size = factor
+    kernel_v = np.zeros((kernel_size, kernel_size))
+
+    if orientation == "horizontal":
+        kernel_h = np.copy(kernel_v)
+        kernel_h[int((kernel_size - 1)/2), :] = np.ones(kernel_size)
+        kernel_h /= kernel_size
+        mb = cv2.filter2D(img, -1, kernel_h)
+    elif orientation == "vertical":
+        kernel_v[:, int((kernel_size - 1)/2)] = np.ones(kernel_size)
+        kernel_v /= kernel_size
+        mb = cv2.filter2D(img, -1, kernel_v)
+    else:
+        print("Error! Must select blur orientation: \"vertical\" or \"horizontal\".")
+        exit(1)
+    return mb
 
 # Change colour of whole picture. USing HSV to only change the colour
 # Don't know how good it is, gets rather blurry around the edges
@@ -46,13 +64,15 @@ def change_colour(img, colour_add):
     #cv2.cvtColor(img_hsv, cv2.COLOR_HSV2BGR_FULL, dst=img_hsv)
     return img_hsv
 
-#IMGDIR = "data/kaggle/3003"
-#img = cv2.imread(os.path.join(IMGDIR,"0001" + ".png"))
-#img_flipped = flip_horizontal(img)
-#img_noise = add_noise(img, 0, 30)
-#img_blur = gaussian_blur(img)
-#img_brighter = change_brigthness(img, 1.2)
-#img_colour = change_colour(img, [200, 50, 0])
+IMGDIR = "data/kaggle/3003"
+img = cv2.imread(os.path.join(IMGDIR,"0001" + ".png"))
+img_flipped = flip_horizontal(img)
+img_noise = add_noise(img, 0, 30)
+img_blur = gaussian_blur(img)
+img_brighter = change_brigthness(img, 1.2)
+img_colour = change_colour(img, [200, 50, 0])
+img_mb = motion_blur(img, "vertical")
 
-#closing all open windows 
-#cv2.destroyAllWindows() 
+cv2.imshow("image",img_mb)
+cv2.waitKey(0)
+cv2.destroyAllWindows() 
--- generate_dataset.py
+++ generate_dataset.py
@@ -34,37 +34,24 @@ bboxes = pd.read_csv("data/test/kaggle_bbox.csv")
 
 
 def write_to_file(image, filename):
-
-    # Save the image to the correct place
     cv2.imwrite(os.path.join(WRITEDIR,filename), image)
 
-    # Add in csv all labels and boxes in this image
-   # with open(LABELCSV, 'w', newline='\n') as csvfile:
-    #    writer = csv.writer(csvfile, delimiter=' ')
 
-    #    for box in label_boxes:
-    #        writer.writerow([filename, box])
 
 # Function for generation one image
 def generate_image_from_list(background_name, images, colour="grey"):
-
-    # Fetch background
     background = cv2.imread(os.path.join(BACKDIR,background_name))
-    # Resize background to 600*400
     background = cv2.resize(background, (WIDTH, HIGTH), interpolation=cv2.INTER_AREA)
-    back_width = background.shape[1]
-    back_height = background.shape[0]
+    back_width = WIDTH
+    back_height = HIGTH
     max_index = 400
 
     # Percentage overlap in x- resp y- directions
     max_overlap = 0
     boxes = []
     for image in images:
-            #lego_scale_factor = random.uniform(0.2, 0.5)
-
             # Find random image of the specific piece
             take_kaggle = random.randint(0, KAGGLE_RATIO)
-     
      
             if(take_kaggle == 0):
                 # Pick raw image
@@ -75,20 +62,15 @@ def generate_image_from_list(background_name, images, colour="grey"):
                 
                 path = os.path.join(DATADIR_RAW,image,filename)
             else:
-                # Set colour for thses
                 colour = "random"
                 rnd_index = random.randint(1, max_index)
 
-                # Strange name in these
                 if(image == "3022"):
-                    filename = "201706161906-" + num_to_namestring(rnd_index) + ".png"
-                else:
                     filename = num_to_namestring(rnd_index) + ".png"
 
                 path = os.path.join(DATADIR_KAGGLE,image,filename)
 
             bbox = bboxes.loc[(bboxes['filename'] == filename) & (bboxes['label'] == int(image))]
-            #print(path)
             img = cv2.imread(path)
 
             # Scale image. Want random between maybe 1/20 and 1/5 of image size? 
@@ -96,10 +78,7 @@ def generate_image_from_list(background_name, images, colour="grey"):
             lego_scale_factor = lego_height/img.shape[0]
             lego_width = int(lego_scale_factor*img.shape[1])
 
-            #width = int(img.shape[1] *lego_scale_factor)
-            #height = int(img.shape[0] *lego_scale_factor)
             dim = (lego_width, lego_height)
-            #print(dim)
 
             # Augment lego piece
             img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
@@ -112,15 +91,12 @@ def generate_image_from_list(background_name, images, colour="grey"):
             else:
                 img = augment_data.change_colour(img, colour)
 
-
-            #img = augment_data.add_noise(img, 0, 30)
             # Scale bounding boxes
             x_low = int(bbox["x_low"]*lego_scale_factor)
             y_low = int(bbox["y_low"]*lego_scale_factor)
             x_high = int(bbox["x_high"]*lego_scale_factor)
             y_high = int(bbox["y_high"]*lego_scale_factor)
 
-            #boxes.append([label, x_low, y_low, x_high, y_high])
             # Make sure the whole box is within background (could change later if we want half pieces)
             # Note: offset for bounding box
             offset_x = np.random.randint(0, back_width - x_high)
@@ -160,8 +136,6 @@ def generate_image_from_list(background_name, images, colour="grey"):
             y_low = offset_y
             boxes.append([image, x_low, y_low, x_high, y_high])
     
-    # Some augmentation on final image. 
-
     # Always blur a little to remove lines between background and lego pieces
     background = augment_data.blur(background)
 
@@ -177,41 +151,29 @@ def generate_image_from_list(background_name, images, colour="grey"):
 # List of images as strings
 def build_dataset(backgrounds, images, size):
     label_boxes = pd.DataFrame(columns=["Image name", "Label", "X-low", "Y-low", "X-high", "Y-high"])
-    # For each image we want to construct
     for i in range(size):
-        
-        # Pick a random background
-        back = random.choice(backgrounds)
-
-        # Select pieces at random for this image
+        background = random.choice(backgrounds)
         num_of_elements = random.randint(MIN_PER_IMAGE, MAX_PER_IMAGE)
         elements = random.choices(images, k=num_of_elements)
 
-        image, boxes = generate_image_from_list(back, elements, colour="grey")
+        image, boxes = generate_image_from_list(background, elements, colour="grey")
         filename = str(i) + FORMAT
         write_to_file(image, filename)
         for box in boxes:
             label_boxes = label_boxes.append({"Image name":filename,"Label":box[0],
                                             "X-low":box[1],"Y-low":box[2],
                                             "X-high":box[3],"Y-high":box[4]}, ignore_index=True)
-        #cv2.imshow(image)
 
         if (i % 100 == 0):
             print("Image number: " + str(i) + " finished")
-    # Save boxes
-    #print(label_boxes)
     label_boxes.to_csv(LABELCSV, index=False)
-    return
     
+
+
+
 images = ["3003", "3004", "3022", "3023"]
-
-#backgrounds = ["carpet", "grass", "legotechnic", "legotechnic2", "treefloor"]
-
-# All images in background folder
 backgrounds = os.listdir(BACKDIR)
 build_dataset(backgrounds, images, 1000)
-#background_name = random.choice(backgrounds)
-#img, boxes = generate_image_from_list(background_name, images, colour="grey")
 
 #cv2.imshow("result", img)
 #cv2.waitKey(0)
